4fd2868ac45f01da82df7c55946c51fbc00e00a8
Ali Sharkasi <alisharkas@gmail.com>
2021-07-24 22:35:33 +0300

    initial Xcode project

---
+++ dev-profile/dev-profile/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  dev-profile
+//
+//  Created by Ali Sharkasi on 24/07/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
